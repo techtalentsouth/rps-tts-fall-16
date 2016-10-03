c5f05e6a2e2f5b683c45fcd50c4230a4e470cbe6
lkitto <lkitto@gsu.edu>
2016-10-03 10:28:09 -0400

    merging hw assignments

---
+++ email_app_LK/lesson_2_challenges_LK/beer.js
@@ -0,0 +1,15 @@
+var bottles = 99;
+var bottles2 = 99;
+
+while (bottles >= 0) {
+    console.log(bottles + " on the wall!");
+    bottles--;
+}
+
+console.log("Hey! We need more beer!");
+console.log('\n One more time!!! \n\n');
+
+for (bottles2; bottles2 >= 0; bottles2--) {
+    console.log(bottles2 + " on the wall!");
+}
+console.log("Hey! We need more beer!");
---
+++ email_app_LK/lesson_2_challenges_LK/coffee.js
@@ -0,0 +1,9 @@
+var coffee = 3;
+if( coffee < 3){
+    console.log("Yes, I'll have another")
+}else{
+    console.log("No, I'm good.")
+}
+/**
+ * Created by lkittogsuedu on 9/27/16.
+ */
